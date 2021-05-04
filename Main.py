9355817d5d7184a34c8f647bf72eb45351ab48ff
Davide <davide.perrone@sns.it>
2021-05-04 14:35:07 +0200

    Rimosso duplicato di ClasseReticolo.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ Programma principale per la generazione di dati sul modello di Ising
 """
 
 import numpy as np
-from isinglib import ClasseReticolo
+from isinglib import ClasseReticolo as reticolo
 #from isinglib import bootstrap
 
 beta = input("Inserisci la temperatura iniziale: ")
@@ -11,15 +11,13 @@ L = input("Inserisci la dimensione del reticolo: ")
 
 ncamp = input("Inserisci il numero di misure: ")
 
-bootstrap.printseed()
 
 bootstrap.setseed(10)
 
-bootstrap.printseed()
 
 lattice = reticolo.Reticolo(L, 1)
-print(lattice.mat)
 
+print(lattice.mat)
 print(lattice.__doc__)
 
 
