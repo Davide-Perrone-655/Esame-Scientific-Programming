437f66f3655da0c7c905c9605ece5f0ced656893
Davide <davide.perrone@sns.it>
2021-05-05 22:47:20 +0200

    Prova di grafico live

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 """
 Programma principale per la generazione di dati sul modello di Ising
 """
-
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from isinglib import bootstrap as bts
@@ -9,28 +9,39 @@ from isinglib import grafico as grf
 from isinglib import classe_reticolo as ret
 #from isinglib import bootstrap
 
-beta=np.linspace(0.30, 0.55, 20)
+beta=np.linspace(0.25, 0.55, 20)
 #print(beta)
-L = 10
+L = 15
 nstep = 100
 nspazzate = 1
 bin_vec = 10
 y=[]
 dy=[]
 lattice = ret.Reticolo(L, beta[0], seed=10)
-print(lattice.mat)
+
+grf.grafico_live(lattice, beta, nstep, nspazzate)
+
+"""
+plt.ion()
+figure, ax = plt.subplots(figsize=(10, 8))
 
 for i in beta:
     lattice.gen_exp(i)
     A = bts.simulazione(lattice, nstep = nstep, nspazzate = nspazzate, bin_vec = bin_vec, nome = 'chi')
     y.append(A['valore'])
     dy.append(A['errore'])
+    ax.matshow(lattice.mat)
+    figure.canvas.draw()
+    figure.canvas.flush_events()
+    time.sleep(0.1)
 
 plt.errorbar(beta, y, dy)
 plt.xlabel('Beta')
 plt.ylabel('Mag')
 plt.grid()
 plt.show()
+"""
+
 #ciao
 #print(magn['valore'],'+-', magn['errore'])
 
--- isinglib/grafico.py
+++ isinglib/grafico.py
@@ -1,8 +1,49 @@
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 from isinglib import bootstrap as bts
 
+
+def grafico_completo(obj_reticolo, beta, nstep, nspazzate, bin_vec, nome):
+    #da riguardare
+    plt.ion()
+    figure, ax = plt.subplots(figsize=(10, 8))
+
+    for i in beta:
+        obj_reticolo.gen_exp(i)
+        A = bts.simulazione(obj_reticolo, nstep = nstep, nspazzate = nspazzate, bin_vec = bin_vec, nome = nome)
+        y.append(A['valore'])
+        dy.append(A['errore'])
+        ax.matshow(obj_reticolo.mat)
+        figure.canvas.draw()
+        figure.canvas.flush_events()
+        time.sleep(0.1)
+    plt.errorbar(beta, y, dy)
+    plt.xlabel('Beta')
+    plt.ylabel('Mag')
+    plt.grid()
+    plt.show()
+
+
+
+
+def grafico_live(obj_reticolo, beta, nstep, nspazzate):
+    plt.ion()
+    figure, ax = plt.subplots(figsize=(10, 8))
+    for i in beta:
+        obj_reticolo.gen_exp(i)
+        for _ in range(nstep):
+            obj_reticolo.aggiorna(nspazzate)
+        ax.matshow(obj_reticolo.mat)
+        figure.canvas.draw()
+        figure.canvas.flush_events()
+        time.sleep(0.1)
+
+
+
+
 def grafico(L, beta_v, nstep, nspazzate, bin=1, nome='|m|'):
+    #da riguardare
     y=[]
     dy=[]
     for i in beta_v:
